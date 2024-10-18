afb422c513fbc176684fb3f52cb7afab94d77bb8
Pedro Artur <arturjovanuzzi@outlook.com>
2024-10-18 17:20:16 -0300

    Update HomePage.tsx

--- frontend/app/src/pages/home/HomePage.tsx
+++ frontend/app/src/pages/home/HomePage.tsx
@@ -12,14 +12,16 @@ export const HomePage = () => {
   return (
     <div>
       <NavBar/>
-      <div className='grid grid-cols-1 grid-rows-[150px_150px_300px] justify-items-center items-center h-screen'>
+      <div className='grid grid-cols-1 grid-rows-[150px_150px_300px] justify-items-center items-center h-screen gap-5'>
         <h1 className='col-span-1 font-semibold sm:place-self-start sm:ml-40 sm:mt-10 text-2xl'>Home</h1>
         <h2 className='col-span-1'>Por Enquanto Não Temos Nada Aqui</h2>
         <HiCheckCircle className='size-40 col-span-1 self-start text-green-500' />
+        <div className='flex justify-center flex-col items-center gap-5'>
         <h2>Links da nossas outras Paginas</h2>
-        <a className='bg-green-500 text-black border-black hover:border-2 transition-colors hover:bg-green-300 w-1/6 rounded-md  p-2' href="https://apimk.wiptelecomunicacoes.com.br/">Pix</a>
-        <a className='bg-green-500 text-black border-black hover:border-2 transition-colors hover:bg-green-300 w-1/6 rounded-md  p-2' href="https://wippainel.wiptelecomunicacoes.com.br/">Wip Painel</a>
-        <a className='bg-green-500 text-black border-black hover:border-2 transition-colors hover:bg-green-300 w-1/6 rounded-md  p-2 mb-10' href="https://whatsemail.wiptelecomunicacoes.com.br/">Mensagens Whatsapp</a>
+        <a className='bg-green-500 text-black border-black hover:border-2 transition-colors hover:bg-green-300 w-full rounded-md  p-2' href="https://apimk.wiptelecomunicacoes.com.br/">Pix</a>
+        <a className='bg-green-500 text-black border-black hover:border-2 transition-colors hover:bg-green-300 w-full rounded-md  p-2' href="https://wippainel.wiptelecomunicacoes.com.br/">Wip Painel</a>
+        <a className='bg-green-500 text-black border-black hover:border-2 transition-colors hover:bg-green-300 w-full rounded-md  p-2 mb-10' href="https://whatsemail.wiptelecomunicacoes.com.br/">Mensagens Whatsapp</a>
+        </div>
       </div>
     </div>
   )
