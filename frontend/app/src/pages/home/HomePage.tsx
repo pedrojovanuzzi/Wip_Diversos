4a7a19f2a9908b036ae74e67852d2f677b7af98a
Pedro Artur <arturjovanuzzi@outlook.com>
2024-10-18 17:10:07 -0300

    Update HomePage.tsx

--- frontend/app/src/pages/home/HomePage.tsx
+++ frontend/app/src/pages/home/HomePage.tsx
@@ -17,9 +17,9 @@ export const HomePage = () => {
         <h2 className='col-span-1'>Por Enquanto Não Temos Nada Aqui</h2>
         <HiCheckCircle className='size-40 col-span-1 self-start text-green-500' />
         <h2>Links da nossas outras Paginas</h2>
-        <a className='bg-green-500 text-black border-black hover:border-2 hover:bg-green-300 w-1/6 rounded-md  p-2' href="https://apimk.wiptelecomunicacoes.com.br/">Pix</a>
-        <a className='bg-green-500 text-black border-black hover:border-2 hover:bg-green-300 w-1/6 rounded-md  p-2' href="https://wippainel.wiptelecomunicacoes.com.br/">Wip Painel</a>
-        <a className='bg-green-500 text-black border-black hover:border-2 hover:bg-green-300 w-1/6 rounded-md  p-2 mb-10' href="https://whatsemail.wiptelecomunicacoes.com.br/">Mensagens Whatsapp</a>
+        <a className='bg-green-500 text-black border-black hover:border-2 transition-colors hover:bg-green-300 w-1/6 rounded-md  p-2' href="https://apimk.wiptelecomunicacoes.com.br/">Pix</a>
+        <a className='bg-green-500 text-black border-black hover:border-2 transition-colors hover:bg-green-300 w-1/6 rounded-md  p-2' href="https://wippainel.wiptelecomunicacoes.com.br/">Wip Painel</a>
+        <a className='bg-green-500 text-black border-black hover:border-2 transition-colors hover:bg-green-300 w-1/6 rounded-md  p-2 mb-10' href="https://whatsemail.wiptelecomunicacoes.com.br/">Mensagens Whatsapp</a>
       </div>
     </div>
   )
