068e553bac5b8a231d5fb2b0af95b7eef8c15e9f
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-01-24 15:38:36 -0300

    Update PDFNFSE.tsx

--- frontend/app/src/pages/Nfe/Components/PDFNFSE.tsx
+++ frontend/app/src/pages/Nfe/Components/PDFNFSE.tsx
@@ -11,6 +11,58 @@ const PDFNFSE = forwardRef<HTMLDivElement, PDFNFSEProps>(({ dados }, ref) => {
     <div className="hidden print:block" ref={ref}>
       {dados.map((item, i) => (
         <div key={i}>
+          {/* // Municipio de Arealva{" "} */}
+          <div className="ring-1 text-sm ring-black grid grid-cols-6">
+            <div className="flex flex-col justify-center relative border-x p-5 col-span-6 border-black">
+              <p className="text-center self-center absolute top-2">
+                Municipio de Arealva
+              </p>
+            </div>
+          </div>
+          {/* Informações Fiscais */}
+          <div>
+          <p className="text-center self-center my-5">
+                NOTA FISCAL DE SERVIÇOS ELETRÔNICA - NFS-e
+              </p>
+              <p>
+              <h1 className="bg-slate-600 w-screen indent-2 p-2 text-gray-200">Informações Fiscais</h1>
+                <div className="grid grid-cols-3 m-5">
+                    <div className="grid-span-1">
+                      <p>Exigibilidade do ISS:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                      <p>Número do Processo:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                      <p>Município de Incidência do ISS:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                      <p>Local da Prestação:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                    </div>
+                    <div className="grid-span-1">
+                      <p>Número do RPS:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                      <p>Série do RPS:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                      <p>Tipo do RPS:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                      <p>Data do RPS:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                      <p>Competência:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                    </div>
+                    <div className="grid-span-1">
+                      <p>Optante Simples Nacional:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                      <p>Incentivo Fiscal:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                      <p>Regime Especial Tributação:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                      <p>Tipo ISS:<br></br> <strong>{item.data?.CompNfse.Nfse.InfNfse.DeclaracaoPrestacaoServico.InfDeclaracaoPrestacaoServico
+                      .Servico.ExigibilidadeISS}</strong></p>
+                    </div>
+
+                </div>
+              </p>
+          </div>
           // Prestador{" "}
           <div className="ring-1 text-sm ring-black grid grid-cols-6">
             <div className="flex justify-start items-center col-span-1  border-black">
