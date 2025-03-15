ae301bdb17c3d8580c34506bf245ee9c76a14a03
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-03-15 09:48:59 -0300

    Update Filter.tsx

--- frontend/app/src/pages/Nfe/Components/Filter.tsx
+++ frontend/app/src/pages/Nfe/Components/Filter.tsx
@@ -347,6 +347,49 @@ export default function Filter({
                     </label>
                   </div>
                 ))}
+                {filters.servicos.map((option, optionIdx) => (
+                  <div key={option.value} className="flex gap-3">
+                    <div className="flex h-5 shrink-0 items-center">
+                      <div className="group grid size-4 grid-cols-1">
+                        <input
+                          value={option.value}
+                          checked={filter.includes(option.value)}
+                          onChange={() => clickedFilter(option.value)}
+                          id={`outros-${optionIdx}`}
+                          name="outros[]"
+                          type="checkbox"
+                          className="col-start-1 row-start-1 appearance-none rounded border border-gray-300 bg-white checked:border-indigo-600 checked:bg-indigo-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                        />
+                        <svg
+                          fill="none"
+                          viewBox="0 0 14 14"
+                          className="pointer-events-none col-start-1 row-start-1 size-3.5 self-center justify-self-center stroke-white group-has-[:disabled]:stroke-gray-950/25"
+                        >
+                          <path
+                            d="M3 8L6 11L11 3.5"
+                            strokeWidth={2}
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="opacity-0 group-has-[:checked]:opacity-100"
+                          />
+                          <path
+                            d="M3 7H11"
+                            strokeWidth={2}
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            className="opacity-0 group-has-[:indeterminate]:opacity-100"
+                          />
+                        </svg>
+                      </div>
+                    </div>
+                    <label
+                      htmlFor={`plano-${optionIdx}`}
+                      className="text-base text-gray-600 sm:text-sm"
+                    >
+                      {option.label}
+                    </label>
+                  </div>
+                ))}
               </div>
             </fieldset>
             <fieldset className="my-5">
