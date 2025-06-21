d2fc4884facf6fcbaa1c8ad1faf7ca7cfbb14ae4
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-06-21 09:51:38 -0300

    Update ClientAnalytics.tsx

--- frontend/app/src/pages/ClientAnalytics/ClientAnalytics.tsx
+++ frontend/app/src/pages/ClientAnalytics/ClientAnalytics.tsx
@@ -319,21 +319,32 @@ export const ClientAnalytics = () => {
                     </li>
 
                     {!loadingSinal && (
-                      <div>
+                      <div className="flex flex-col sm:flex-row">
                       <button
                         onClick={() => fetchSinal(pppoe)}
                         className="sm:mx-2 my-2 sm:my-0 bg-red-700 text-white px-6 py-2 rounded hover:bg-red-400 transition-all"
                       >
                         Testar Onu Novamente
                       </button>
-                      <button
-                        onClick={() => fetchReset(pppoe)}
-                        className="sm:mx-2 my-2 sm:my-0 bg-blue-700 text-white px-6 py-2 rounded hover:bg-red-400 transition-all"
-                      >
-                        Reiniciar Onu
-                      </button>
+                      {loadingReset ? (
+                        <>
+                        <Spinner text="Reiniciando ONU"/>
+                        </>
+                        
+                      ) : errorLoading ? (
+                        <ErrorMessage message={errorLoading} />
+                      ) : (
+                        <button
+                          onClick={() => fetchReset(pppoe)}
+                          className="bg-blue-700 text-white px-6 py-2 rounded hover:bg-blue-400 transition-all"
+                        >
+                          Reiniciar ONU
+                        </button>
+                      )
+                      }
                       </div>
                     )}
+
                     <div className="p-5 mt-5"></div>
                     <li className="flex">
                       3. Conectado?:{" "}
