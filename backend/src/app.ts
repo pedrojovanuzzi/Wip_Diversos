d9de09f4298327a76433a80a556c0a111dfcc88e
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-08-05 08:17:02 -0300

    Testando Criar Backup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import ChamadosRouter from './routes/Chamados.Routes'; // Caminho correto para o arquivo de rotas
+import ChamadosRouter from "./routes/Chamados.Routes"; // Caminho correto para o arquivo de rotas
 import Home from "./routes/Home.Routes";
 import Auth from "./routes/Auth.Routes";
 import cors from "cors";
@@ -8,23 +8,26 @@ import NFSE from "./routes/NFSE.routes";
 import Whatsapp from "./routes/Whatsapp.Routes";
 import Prefeitura from "./routes/PrefeituraUser.routes";
 import ClientAnalytics from "./routes/ClientAnalytics.routes";
+import cron from "node-cron";
+import Backup from "./controller/Backup"; // Caminho para sua classe de backup
 
-export class App{
+export class App {
   public server: express.Application;
 
-  constructor(){
+  constructor() {
     this.server = express();
     this.middleware();
     this.router();
+    this.agendarBackup();
   }
 
-  private middleware(){
+  private middleware() {
     this.server.use(express.json());
-    this.server.use(express.urlencoded({extended: true}));
-    this.server.use(cors({origin: process.env.URL}));
+    this.server.use(express.urlencoded({ extended: true }));
+    this.server.use(cors({ origin: process.env.URL }));
   }
 
-  private router(){
+  private router() {
     this.server.use("/api/chamados", ChamadosRouter);
     this.server.use("/api/", Home);
     this.server.use("/api/auth", Auth);
@@ -33,5 +36,38 @@ export class App{
     this.server.use("/api/whatsapp", Whatsapp);
     this.server.use("/api/Prefeitura", Prefeitura);
     this.server.use("/api/ClientAnalytics", ClientAnalytics);
+  }
+
+  // private agendarBackup() {
+  //   // 🕒 Agendar para todo dia às 03:00
+  //   cron.schedule("0 3 * * *", async () => {
+  //     console.log("⏰ Executando backup automático", new Date().toLocaleString());
+  //     try {
+  //       await Backup.gerarTodos();
+  //     } catch (err) {
+  //       console.error("❌ Falha no backup agendado:", err);
+  //     }
+  //   });
+
+  //   console.log("📅 Agendador de backup inicializado.");
+  // }
+
+  private agendarBackup() {
+    // 🕐 A cada 1 minuto
+    cron.schedule("* * * * *", async () => {
+      console.log(
+        "⏰ Executando backup automático",
+        new Date().toLocaleString()
+      );
+      try {
+        await Backup.gerarTodos();
+      } catch (err) {
+        console.error("❌ Falha no backup agendado:", err);
+      }
+    });
+
+    console.log(
+      "📅 Agendador de backup inicializado para rodar a cada 1 minuto."
+    );
   }
 }
