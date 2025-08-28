07581d916334ae56ae70aac1a1aacd5b3a653427
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-08-28 15:04:56 -0300

    Modificações em try catchs

--- frontend/app/src/pages/DDDOS_Monitoring/DDDOS_Home.tsx
+++ frontend/app/src/pages/DDDOS_Monitoring/DDDOS_Home.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { TypedUseSelectorHook, useSelector } from "react-redux";
+import { RootState } from "../../types";
 
 export const DDDOS_Home = () => {
-  return (
-    <div>DDDOS_Home</div>
-  )
-}
+  //Redux
+  const useTypedSelector: TypedUseSelectorHook<RootState> = useSelector;
+  const userToken = useTypedSelector((state: RootState) => state.auth.user);
+  const token = userToken.token;
+
+  const [last10pppoe, setLast10Ppoe] = useState("");
+  const [eventsPerMinute, seteventsPerMinute] = useState("");
+  const [eventsPerHost, seteventsPerHost] = useState("");
+
+  useEffect(() => {
+    getLast10();
+  }, []);
+
+  async function getLast10() {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_URL + "/DosProtect/last10Pppoe",
+        {
+          headers: { Authorization: `Bearer ${token}`, timeout: 60000 },
+        }
+      );
+
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  }
+
+  return <div>DDDOS_Home</div>;
+};
