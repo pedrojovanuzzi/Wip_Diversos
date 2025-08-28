9ed04a5817b66307a58699104742fa2778f8032e
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-08-28 15:18:07 -0300

    Get das informações do banco de dados feito

--- frontend/app/src/pages/DDDOS_Monitoring/DDDOS_Home.tsx
+++ frontend/app/src/pages/DDDOS_Monitoring/DDDOS_Home.tsx
@@ -9,18 +9,54 @@ export const DDDOS_Home = () => {
   const userToken = useTypedSelector((state: RootState) => state.auth.user);
   const token = userToken.token;
 
-  const [last10pppoe, setLast10Ppoe] = useState("");
-  const [eventsPerMinute, seteventsPerMinute] = useState("");
-  const [eventsPerHost, seteventsPerHost] = useState("");
+  const [last10pppoe, setLast10Ppoe] = useState<{}>();
+  const [eventsPerMinute, seteventsPerMinute] = useState<{}>();
+  const [eventsPerHost, seteventsPerHost] = useState<{}>();
 
   useEffect(() => {
-    getLast10();
+    setLast10Ppoe(getLast10());
+    seteventsPerHost(geteventsPerHost());
+    seteventsPerMinute(geteventsPerMinute());
   }, []);
 
   async function getLast10() {
     try {
       const response = await axios.get(
         process.env.REACT_APP_URL + "/DosProtect/last10Pppoe",
+        {
+          headers: { Authorization: `Bearer ${token}`, timeout: 60000 },
+        }
+      );
+
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  }
+
+  async function geteventsPerMinute() {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_URL + "/DosProtect/eventsPerMinute",
+        {
+          headers: { Authorization: `Bearer ${token}`, timeout: 60000 },
+        }
+      );
+
+      console.log(response);
+      return response;
+    } catch (error) {
+      console.log(error);
+      return;
+    }
+  }
+
+  async function geteventsPerHost() {
+    try {
+      const response = await axios.get(
+        process.env.REACT_APP_URL + "/DosProtect/eventsPerHost",
         {
           headers: { Authorization: `Bearer ${token}`, timeout: 60000 },
         }
