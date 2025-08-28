3db74f961667b60821a192723387e1e7d8953c1f
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-08-28 15:49:46 -0300

    Criando Graficos

--- frontend/app/src/pages/DDDOS_Monitoring/DDDOS_Home.tsx
+++ frontend/app/src/pages/DDDOS_Monitoring/DDDOS_Home.tsx
@@ -2,6 +2,30 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import { TypedUseSelectorHook, useSelector } from "react-redux";
 import { RootState } from "../../types";
+import {
+  ResponsiveContainer,
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+} from "recharts";
+
+interface Ilast10 {
+  pppoe: string;
+  total: number;
+}
+
+interface IeventsPerMinute {
+  minuto: string;
+  total: number;
+}
+
+interface IeventsPerHost {
+  host: string;
+  total: number;
+}
 
 export const DDDOS_Home = () => {
   //Redux
@@ -9,17 +33,24 @@ export const DDDOS_Home = () => {
   const userToken = useTypedSelector((state: RootState) => state.auth.user);
   const token = userToken.token;
 
-  const [last10pppoe, setLast10Ppoe] = useState<{}>();
-  const [eventsPerMinute, seteventsPerMinute] = useState<{}>();
-  const [eventsPerHost, seteventsPerHost] = useState<{}>();
+  const [last10pppoe, setLast10Ppoe] = useState<Ilast10 | undefined>();
+  const [eventsPerMinute, seteventsPerMinute] = useState<
+    IeventsPerMinute | undefined
+  >();
+  const [eventsPerHost, seteventsPerHost] = useState<
+    IeventsPerHost | undefined
+  >();
 
   useEffect(() => {
-    setLast10Ppoe(getLast10());
-    seteventsPerHost(geteventsPerHost());
-    seteventsPerMinute(geteventsPerMinute());
+    const start = async () => {
+      setLast10Ppoe(await getLast10());
+      seteventsPerHost(await geteventsPerHost());
+      seteventsPerMinute(await geteventsPerMinute());
+    };
+    start();
   }, []);
 
-  async function getLast10() {
+  async function getLast10(): Promise<Ilast10 | undefined> {
     try {
       const response = await axios.get(
         process.env.REACT_APP_URL + "/DosProtect/last10Pppoe",
@@ -29,14 +60,14 @@ export const DDDOS_Home = () => {
       );
 
       console.log(response);
-      return response;
+      return { pppoe: response.data.pppoe, total: response.data.total };
     } catch (error) {
       console.log(error);
       return;
     }
   }
 
-  async function geteventsPerMinute() {
+  async function geteventsPerMinute(): Promise<IeventsPerMinute | undefined> {
     try {
       const response = await axios.get(
         process.env.REACT_APP_URL + "/DosProtect/eventsPerMinute",
@@ -46,14 +77,14 @@ export const DDDOS_Home = () => {
       );
 
       console.log(response);
-      return response;
+      return { minuto: response.data.minuto, total: response.data.total };
     } catch (error) {
       console.log(error);
       return;
     }
   }
 
-  async function geteventsPerHost() {
+  async function geteventsPerHost(): Promise<IeventsPerHost | undefined> {
     try {
       const response = await axios.get(
         process.env.REACT_APP_URL + "/DosProtect/eventsPerHost",
@@ -63,12 +94,23 @@ export const DDDOS_Home = () => {
       );
 
       console.log(response);
-      return response;
+      return { host: response.data.host, total: response.data.total };
     } catch (error) {
       console.log(error);
       return;
     }
   }
 
-  return <div>DDDOS_Home</div>;
+  return <div>
+    <div className="w-full h-64">
+      <ResponsiveContainer width={'100%'} height={'100%'}>
+        <LineChart data={last10pppoe as any}>
+        <CartesianGrid strokeDasharray={'3 3'}/>
+        <XAxis dataKey={last10pppoe?.pppoe}></XAxis>
+        <Tooltip/>
+        <Line type={'monotone'} dataKey={last10pppoe?.total} stroke="#10B981" strokeWidth={2} dot={false}/>
+        </LineChart>
+      </ResponsiveContainer>
+    </div>
+  </div>;
 };
