cbbdd4322e30e58da8d1ce1da6955680f037528e
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-09-02 10:27:51 -0300

    Update cancelarNfse.ts

--- backend/src/cancelarNfse.ts
+++ backend/src/cancelarNfse.ts
@@ -4,8 +4,56 @@ import forge from "node-forge";
 import { create } from "xmlbuilder2";
 import { SignedXml } from "xml-crypto";
 import path from "path";
+import * as fs from "fs";
+import * as os from "os";
+import { execSync } from "child_process";
+
 
 dotenv.config({ path: path.join(__dirname, "..", ".env") });
+
+
+
+
+export function processarCertificado(certPath: string, password: string, tempDir: string) {
+  if (!fs.existsSync(tempDir)) fs.mkdirSync(tempDir, { recursive: true });
+
+  const DECRYPTED_CERT_PATH = path.join(tempDir, "decrypted_certificado.tmp");
+  const NEW_CERT_PATH = path.join(tempDir, "new_certificado.pfx");
+
+  const isLinux = os.platform() === "linux";
+  const isWindows = os.platform() === "win32";
+
+  try {
+    if (isLinux) {
+      execSync(
+        `openssl pkcs12 -in "${certPath}" -nodes -legacy -passin pass:${password} -out "${DECRYPTED_CERT_PATH}"`,
+        { stdio: "inherit" }
+      );
+      execSync(
+        `openssl pkcs12 -in "${DECRYPTED_CERT_PATH}" -export -out "${NEW_CERT_PATH}" -passout pass:${password}`,
+        { stdio: "inherit" }
+      );
+    } else if (isWindows) {
+      const powershellCommand = `
+            try {
+              $certificado = New-Object System.Security.Cryptography.X509Certificates.X509Certificate2('${certPath}', '${password}', [System.Security.Cryptography.X509Certificates.X509KeyStorageFlags]::Exportable);
+              $bytes = $certificado.Export([System.Security.Cryptography.X509Certificates.X509ContentType]::Pkcs12, '${password}');
+              [System.IO.File]::WriteAllBytes('${NEW_CERT_PATH}', $bytes)
+            } catch {
+              Write-Error $_.Exception.Message
+              exit 1
+            }
+          `;
+      execSync(`powershell -Command "${powershellCommand.replace(/\n/g, " ")}"`, { stdio: ["ignore", "inherit", "pipe"] });
+    }
+
+    return NEW_CERT_PATH;
+  } catch (error) {
+    console.error("❌ Erro ao processar o certificado:", error);
+    return certPath; // Retorna o original caso falhe
+  }
+}
+
 
 
 const CERT_PATH = path.join(__dirname, '/files', 'certificado.pfx');
@@ -89,8 +137,8 @@ const cancelar = body.ele("ws:cancelarNfse");
 
 cancelar.import(signedBuilder);
 
-cancelar.ele("username").txt("20843290000142");
-cancelar.ele("password").txt("32961608");
+cancelar.ele("username").txt(process.env.MUNICIPIO_LOGIN as string);
+cancelar.ele("password").txt(process.env.MUNICIPIO_SENHA as string);
 
 const finalXml = envelope.end({ prettyPrint: true });
 writeFileSync("cancelamento-assinado.xml", finalXml);
