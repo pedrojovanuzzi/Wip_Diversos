69510f052543619285638c70e601f23dd3d46fc9
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-09-16 15:53:22 -0300

    Criando Função de Pdf

--- backend/src/controller/PowerDns.ts
+++ backend/src/controller/PowerDns.ts
@@ -5,8 +5,14 @@ import {Request, Response} from "express";
 class PowerDNS{
 
     public async inserirPdf(req: Request, res: Response){
-        // pdfUtils();
+        console.log(req.file);
+        this.transformarPdf();
     }
+
+    private async transformarPdf(){
+
+    }
+
 }
 
 export default PowerDNS;
--- backend/src/routes/PowerDns.routes.ts
+++ backend/src/routes/PowerDns.routes.ts
@@ -2,12 +2,18 @@ import {Router} from "express"
 import AuthGuard from "../middleware/AuthGuard";
 import PowerDNS from "../controller/PowerDns";
 
+import path from "path";
+import multer from "multer";
+
+const upload = multer({ dest: path.join(__dirname, "..", "uploads") });
+
+
 const powerdns = new PowerDNS();
 
 // import 
 const router: Router = Router();
 
-router.get("/inserirPdf", AuthGuard, powerdns.inserirPdf);
+router.post("/inserirPdf", AuthGuard, upload.single("file"), powerdns.inserirPdf.bind(powerdns));
 
 
 export default router;
