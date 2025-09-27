eaa628cbdb130af36d15d19c58697e76f45be509
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-09-27 11:14:31 -0300

    Desoutorizar Onu

--- backend/src/routes/Onu.routes.ts
+++ backend/src/routes/Onu.routes.ts
@@ -9,6 +9,7 @@ const router: Router = Router();
 
 router.post("/OnuAuthenticationBridge", AuthGuard, onu.onuAuthenticationBridge);
 router.post("/OnuAuthenticationWifi", AuthGuard, onu.onuAuthenticationWifi);
+router.post("/Desautorize", AuthGuard, onu.Desautorize);
 router.post("/OnuShowOnline", AuthGuard, onu.onuShowOnline);
 router.post("/OnuShowAuth", AuthGuard, onu.onuShowAuth);
 router.post("/querySn", AuthGuard, onu.querySn);
--- frontend/app/src/pages/onu/DesautorizaOnu.tsx
+++ frontend/app/src/pages/onu/DesautorizaOnu.tsx
@@ -9,22 +9,11 @@ export const DesautorizaOnu = () => {
   const { user } = useAuth();
   const token = user?.token;
 
-  const [bridge, setBridge] = useState(true);
-  const [variasOnus, setvariasOnus] = useState(true);
   const [sn, setSn] = useState("");
   const [loading, setLoading] = useState(false);
   const [sucesso, setSucesso] = useState('');
   const [error, setError] = useState("");
-  const [vlan, setVlan] = useState("");
-  const [cos, setCos] = useState("");
-  const [wifiData, setWifiData] = useState<WifiData>({
-    pppoe: "",
-    senha_pppoe: "",
-    canal: "",
-    wifi_2ghz: "",
-    wifi_5ghz: "",
-    senha_wifi: "",
-  });
+
 
   const saved = localStorage.getItem('sn');
 
@@ -40,11 +29,9 @@ export const DesautorizaOnu = () => {
 
     try {
       setLoading(true);
-      if (bridge) {
-        // modo Bridge
-        const response = await axios.post(
-          `${process.env.REACT_APP_URL}/Onu/OnuAuthenticationBridge`,
-          { sn, vlan, cos },
+      const response = await axios.post(
+          `${process.env.REACT_APP_URL}/Onu/Desautorize`,
+          { sn },
           {
             headers: {
               Authorization: `Bearer ${token}`,
@@ -55,21 +42,6 @@ export const DesautorizaOnu = () => {
         );
         console.log(response.data);
         setSucesso(response.data)
-      } else {
-        const response = await axios.post(
-          `${process.env.REACT_APP_URL}/Onu/OnuAuthenticationWifi`,
-          { sn, vlan, cos, wifiData },
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-              "Content-Type": "application/json",
-            },
-            timeout: 60000,
-          }
-        );
-        console.log(response.data);
-        setSucesso(response.data)
-      }
     } catch (error : any) {
       console.error(error);
       setError(error);
@@ -99,116 +71,17 @@ export const DesautorizaOnu = () => {
             <input
               value={sn}
               onChange={(e) => setSn(e.target.value)}
-              placeholder={`${!variasOnus ? 'FHTT0726a260' : 'FHTT0726a260, FHTTfe1aca8b'}`}
+              placeholder={`FHTT0726a260`}
               required
               className="block w-full rounded-md border px-3 py-1.5"
             />
 
-            <label className="block text-sm font-medium text-gray-900">VLAN</label>
-            <input
-              value={vlan}
-              onChange={(e) => setVlan(e.target.value)}
-              placeholder="Ex: 1008"
-              required
-              className="block w-full rounded-md border px-3 py-1.5"
-            />
-
-            <label className="block text-sm font-medium text-gray-900">Cos</label>
-            <input
-              value={cos}
-              onChange={(e) => setCos(e.target.value)}
-              placeholder="Cos"
-              required
-              className="block w-full rounded-md border px-3 py-1.5"
-            />
-
-            {/* Se Wifi for selecionado */}
-            {!bridge && (
-              <>
-                <label className="block text-sm font-medium text-gray-900">PPPOE</label>
-                <input
-                  value={wifiData.pppoe}
-                  placeholder="PEDROJOVANUZZI"
-                  onChange={(e) =>
-                    setWifiData({ ...wifiData, pppoe: e.target.value })
-                  }
-                  required
-                  className="block w-full rounded-md border px-3 py-1.5"
-                />
-
-                <label className="block text-sm font-medium text-gray-900">
-                  Senha PPPOE
-                </label>
-                <input
-                  type="text"
-                  value={wifiData.senha_pppoe}
-                  onChange={(e) =>
-                    setWifiData({ ...wifiData, senha_pppoe: e.target.value })
-                  }
-                  placeholder="270604"
-                  required
-                  className="block w-full rounded-md border px-3 py-1.5"
-                />
-
-                <label className="block text-sm font-medium text-gray-900">Canal</label>
-                <input
-                placeholder="3"
-                  value={wifiData.canal}
-                  onChange={(e) =>
-                    setWifiData({ ...wifiData, canal: e.target.value })
-                  }
-                  required
-                  className="block w-full rounded-md border px-3 py-1.5"
-                />
-
-                <label className="block text-sm font-medium text-gray-900">
-                  Nome da Rede Wifi 2.4
-                </label>
-                <input
-                  value={wifiData.wifi_2ghz}
-                  onChange={(e) =>
-                    setWifiData({ ...wifiData, wifi_2ghz: e.target.value })
-                  }
-                  placeholder="Wifi_Test"
-                  required
-                  className="block w-full rounded-md border px-3 py-1.5"
-                />
-
-                <label className="block text-sm font-medium text-gray-900">
-                  Nome da Rede Wifi 5.0
-                </label>
-                <input
-                  value={wifiData.wifi_5ghz}
-                  onChange={(e) =>
-                    setWifiData({ ...wifiData, wifi_5ghz: e.target.value })
-                  }
-                  placeholder="Wifi_Test_5G"
-                  required
-                  className="block w-full rounded-md border px-3 py-1.5"
-                />
-
-                <label className="block text-sm font-medium text-gray-900">
-                  Senha Wifi
-                </label>
-                <input
-                  type="text"
-                  value={wifiData.senha_wifi}
-                  onChange={(e) =>
-                    setWifiData({ ...wifiData, senha_wifi: e.target.value })
-                  }
-                  placeholder="fwgerwhge#"
-                  minLength={7}
-                  required
-                  className="block w-full rounded-md border px-3 py-1.5"
-                />
-              </>
-            )}
 
             <button
               type="submit"
-              className="bg-indigo-600 text-white p-3 rounded-md"
+              className="bg-red-600 text-white p-3 rounded-md"
             >
-              Criar
+              Desautorizar
             </button>
             
           </form>
