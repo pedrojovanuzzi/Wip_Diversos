279de42e334e1a247202299de1a5fa51f3c54d2b
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-10-16 09:50:21 -0300

    Resolvendo Erro da Biblioteca EFI

--- backend/dist/controller/Home.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class Home {
-    show(req, res) {
-        res.json("Initial Page");
-    }
-}
-exports.default = new Home();
