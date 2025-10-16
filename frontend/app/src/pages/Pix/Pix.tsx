9b4757dd0fd732f89181c284c693336a4394a57e
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-10-16 10:59:26 -0300

    Ajuste

--- frontend/app/src/pages/Pix/Pix.tsx
+++ frontend/app/src/pages/Pix/Pix.tsx
@@ -5,43 +5,8 @@ import { ClockIcon } from "@heroicons/react/24/outline";
 import { IoDocuments } from "react-icons/io5";
 import { FaBoxOpen, FaUsers } from "react-icons/fa";
 import { MdOutlineAutoMode } from "react-icons/md";
+import PixCard from "./components/PixCard";
 
-// 🔹 Componente auxiliar (para não repetir estrutura)
-interface PixCardProps {
-  link: string;
-  icon: React.ReactNode;
-  cor: string;
-  titulo: string;
-  descricao: string;
-  spanFull?: boolean;
-}
-
-const PixCard: React.FC<PixCardProps> = ({
-  link,
-  icon,
-  cor,
-  titulo,
-  descricao,
-  spanFull,
-}) => (
-  <div
-    className={`group relative ring-1 ring-gray-400 hover:bg-green-300 transition-all bg-gray-100 p-6 ${
-      spanFull ? "col-span-2" : ""
-    }`}
-  >
-    <span className={`inline-flex rounded-lg p-3 ${cor}`}>{icon}</span>
-    <div className="mt-8">
-      <h3 className="text-base font-semibold text-gray-900">
-        {/* 🔹 Troquei o <a> por <Link> */}
-        <Link to={link} className="focus:outline-none">
-          <span aria-hidden="true" className="absolute inset-0" />
-          {titulo}
-        </Link>
-      </h3>
-      <p className="mt-2 text-sm text-gray-600">{descricao}</p>
-    </div>
-  </div>
-);
 
 
 export const Pix = () => {
