e39a4de5b7199eff7ebc358ddfd1b44a48103535
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-10-22 11:07:39 -0300

    Minor Changes

--- frontend/app/src/pages/Pix/PixAdmin.tsx
+++ frontend/app/src/pages/Pix/PixAdmin.tsx
@@ -63,7 +63,7 @@ const [urlWebhook, setUrlWebhook] = useState('');
   }
 
   return (
-    <div className="p-5 bg-slate-800 min-h-screen text-gray-200">
+    <div className="sm:p-5 bg-slate-800 min-h-screen text-gray-200">
       <NavBar />
       <div className="bg-gray-100 text-gray-900 p-10 rounded-md flex flex-col gap-4 items-center">
         <h1 className="text-2xl font-bold">Pix Automático</h1>
