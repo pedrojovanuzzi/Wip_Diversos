f226444d2713635041525a376ba2bf6801a5b08b
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-11-21 10:07:03 -0300

    Minor Fixes Pix Automatico

--- frontend/app/src/pages/Pix/PixCancelarCobranca.tsx
+++ frontend/app/src/pages/Pix/PixCancelarCobranca.tsx
@@ -3,12 +3,21 @@ import { NavBar } from "../../components/navbar/NavBar";
 import { useAuth } from "../../context/AuthContext";
 import axios from "axios";
 
+import {
+  Dialog,
+  DialogBackdrop,
+  DialogPanel,
+  DialogTitle,
+} from "@headlessui/react";
+import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+
 export const PixCancelarCobranca = () => {
   const [error, setError] = useState("");
   const [loading, setLoading] = useState(false);
   const { user } = useAuth();
   const [tabelaCancelada, setTabelaCancelada] = useState<any>();
   const [txid, setTxId] = useState("");
+  const [openModal, setOpenModal] = useState(false);
   const token = user?.token;
 
   // 🔧 Função que converte qualquer tipo de erro em string segura
@@ -89,7 +98,7 @@ export const PixCancelarCobranca = () => {
           onChange={(e) => setTxId(e.target.value)}
         />
         <button
-          onClick={cancelarCobranca}
+          onClick={() => setOpenModal(true)}
           className=" sm:rounded-md ring-1 p-4 sm:p-2 self-center mt-4 bg-red-800 text-white w-full sm:w-[30vw]"
         >
           Cancelar
@@ -100,6 +109,68 @@ export const PixCancelarCobranca = () => {
         >
           Buscar Cobrança
         </button>{" "}
+        <Dialog
+          open={openModal}
+          onClose={setOpenModal}
+          className="relative z-10"
+        >
+          <DialogBackdrop
+            transition
+            className="fixed inset-0 bg-gray-500/75 transition-opacity data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in"
+          />
+
+          <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+            <div className="flex min-h-full items-end justify-center p-4 text-center items-center sm:p-0">
+              <DialogPanel
+                transition
+                className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all data-[closed]:translate-y-4 data-[closed]:opacity-0 data-[enter]:duration-300 data-[leave]:duration-200 data-[enter]:ease-out data-[leave]:ease-in sm:my-8 sm:w-full sm:max-w-lg sm:p-6 data-[closed]:sm:translate-y-0 data-[closed]:sm:scale-95"
+              >
+                <div className="sm:flex sm:items-start">
+                  <div className="mx-auto flex size-12 shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:size-10">
+                    <ExclamationTriangleIcon
+                      aria-hidden="true"
+                      className="size-6 text-red-600"
+                    />
+                  </div>
+                  <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    <DialogTitle
+                      as="h3"
+                      className="text-base font-semibold text-gray-900"
+                    >
+                      Cancelar Cobrança
+                    </DialogTitle>
+                    <div className="mt-2">
+                      <p className="text-sm text-gray-500">
+                        Tem certeza que deseja cancelar esta cobrança? Essa ação
+                        não pode ser desfeita.
+                      </p>
+                    </div>
+                  </div>
+                </div>
+                <div className="mt-5 sm:mt-4 sm:flex sm:flex-row-reverse">
+                  <button
+                    type="button"
+                    onClick={() => {
+                      cancelarCobranca();
+                      setOpenModal(false);
+                    }}
+                    className="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
+                  >
+                    Cancelar
+                  </button>
+                  <button
+                    type="button"
+                    data-autofocus
+                    onClick={() => setOpenModal(false)}
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                  >
+                    Voltar
+                  </button>
+                </div>
+              </DialogPanel>
+            </div>
+          </div>
+        </Dialog>
         {loading && <p className="mt-2">Carregando ....</p>}
         {error && (
           <p className="text-red-500 break-all self-center w-1/2 mt-2">
