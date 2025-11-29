d03de41208f1f375ab9680a5c212aa9f75165e46
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-11-29 10:28:26 -0300

    Arquivo para Rodar Migrations

---
+++ backend/src/run-migrations.ts
@@ -0,0 +1,26 @@
+import { exec } from "child_process";
+import { promisify } from "util";
+
+const execAsync = promisify(exec);
+
+// npm run migration:run
+
+async function runMigrations() {
+  try {
+    console.log("🚀 Executando migrations...");
+
+    const { stdout, stderr } = await execAsync(
+      "npx ts-node ./node_modules/typeorm/cli.js migration:run -d src/database/DataSource.ts"
+    );
+
+    if (stdout) console.log(stdout);
+    if (stderr) console.error(stderr);
+
+    console.log("✅ Migrations executadas com sucesso!");
+  } catch (error: any) {
+    console.error("❌ Erro ao executar migrations:", error.message);
+    process.exit(1);
+  }
+}
+
+runMigrations();
