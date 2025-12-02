ba81c1bda2801f5a07387a38ae376aa1a97f79e0
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-12-02 14:47:52 -0300

    Update SearchInterface.tsx

--- frontend/app/src/pages/Nfcom/SearchInterface.tsx
+++ frontend/app/src/pages/Nfcom/SearchInterface.tsx
@@ -419,7 +419,7 @@ export default function SearchInterface() {
                         <td className="px-6 py-4 text-left whitespace-nowrap">
                           <span
                             className={`px-2 inline-flex text-xs leading-5 font-semibold rounded-full ${
-                              nfcom.status === "Autorizada"
+                              nfcom.status === "autorizada"
                                 ? "bg-green-100 text-green-800"
                                 : "bg-yellow-100 text-yellow-800"
                             }`}
