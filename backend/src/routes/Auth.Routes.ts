d071e2002b6c716cf198055b66fc5d33ac8d83aa
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-12-11 16:25:09 -0300

    Modificado Tokens

--- backend/src/routes/Auth.Routes.ts
+++ backend/src/routes/Auth.Routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import Auth from "../controller/Auth";
 
-const router: Router = Router()
+const router: Router = Router();
 
 //Routes
 router.get("/", Auth.show);
@@ -9,5 +9,6 @@ router.post("/create", Auth.createUser);
 router.post("/login", Auth.Login);
 router.get("/getUser", Auth.getCurrentUser);
 router.post("/api", Auth.getToken);
+router.post("/validate", Auth.validateToken);
 
 export default router;
