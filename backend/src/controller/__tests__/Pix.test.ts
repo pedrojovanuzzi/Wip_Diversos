feffaf7953f39e8f20c3e0bed691fad5c704bd77
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2025-12-22 10:36:54 -0300

    Update Pix.test.ts

--- backend/src/controller/__tests__/Pix.test.ts
+++ backend/src/controller/__tests__/Pix.test.ts
@@ -20,10 +20,12 @@ describe("Pix Controller", () => {
 
     const controllerAny = pixController as any;
 
-    vi.spyOn(controllerAny.clienteRepo, "findOne").mockResolvedValue({
-      login: "pedro",
-      desconto: 2.0,
-    });
+    const called = vi
+      .spyOn(controllerAny.clienteRepo, "findOne")
+      .mockResolvedValue({
+        login: "pedro",
+        desconto: 2.0,
+      });
 
     const response = await pixController.aplicarJuros_Desconto(
       50,
@@ -31,6 +33,25 @@ describe("Pix Controller", () => {
       "2025-12-22"
     );
 
+    expect(called).toHaveBeenCalled();
     expect(response).toBe(48.96);
+  });
+
+  it("Aplicar desconto", async () => {
+    const pixController = new Pix();
+
+    const controllerAny = pixController as any;
+
+    const called = vi
+      .spyOn(controllerAny.clienteRepo, "findOne")
+      .mockResolvedValue({
+        login: "pedro",
+        desconto: 2.0,
+      });
+
+    const response = await pixController.aplicar_Desconto(35, "pedro");
+
+    expect(called).toHaveBeenCalled();
+    expect(response).toBe(33);
   });
 });
