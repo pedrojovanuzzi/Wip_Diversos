9967d4f7e0fd19486845909252d41712a0a8faf0
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2026-01-05 10:44:36 -0300

    Update NFSE.ts

--- backend/src/controller/NFSE.ts
+++ backend/src/controller/NFSE.ts
@@ -247,7 +247,7 @@ export class NFSEController {
       }
 
       console.log(
-        ">>>>> DEBUG ITEM LISTA SEVICO (MODIFICADO): " +
+        ">>>>> DEBUG ITEM LISTA SEVICO (MODIFICADO): dddddddddd" +
           nfseBase.itemListaServico
       );
 
