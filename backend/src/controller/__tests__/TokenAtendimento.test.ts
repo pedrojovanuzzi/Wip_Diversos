01c0798d5a67412532d00445aad040acbbbd9296
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2026-01-07 14:44:26 -0300

    Criando Token de AutoAtendimento

---
+++ backend/src/controller/__tests__/TokenAtendimento.test.ts
@@ -0,0 +1,11 @@
+describe("TokenAtendimento", () => {
+  it("Deve gerar um pix de pagamento", () => {
+    const cliente = {
+      id: 1,
+      nome: "Teste",
+      cpf: "12345678901",
+      telefone: "12345678901",
+      email: "test@example.com",
+    };
+  });
+});
