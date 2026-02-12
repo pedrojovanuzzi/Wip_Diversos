7626845dd71db5a6d3bce7110b0949b27cfaebe8
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2026-02-12 15:49:39 -0300

    Update Licenca.Routes.ts

--- backend/src/routes/Licenca.Routes.ts
+++ backend/src/routes/Licenca.Routes.ts
@@ -1,14 +1,15 @@
 import { Router } from "express";
 import LicencaController from "../controller/LicencaController";
+import AuthGuard from "../middleware/AuthGuard";
 
 const Licenca = Router();
 
-Licenca.post("/criar", LicencaController.criarLicenca);
-Licenca.get("/listar", LicencaController.listarLicencas);
-Licenca.put("/status/:id", LicencaController.atualizarStatus);
+Licenca.post("/criar", AuthGuard, LicencaController.criarLicenca);
+Licenca.get("/listar", AuthGuard, LicencaController.listarLicencas);
+Licenca.put("/status/:id", AuthGuard, LicencaController.atualizarStatus);
 Licenca.get("/verificar", LicencaController.verificarLicenca); // GET para consulta simples
 Licenca.post("/verificar", LicencaController.verificarLicenca); // POST para enviar dados mais complexos se precisar
 Licenca.post("/recuperar-chave", LicencaController.recuperarChaveLicenca);
-Licenca.delete("/:id", LicencaController.removerLicenca);
+Licenca.delete("/:id", AuthGuard, LicencaController.removerLicenca);
 
 export default Licenca;
