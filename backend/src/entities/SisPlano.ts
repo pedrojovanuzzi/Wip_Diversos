83b1504643d2917d563562123faac3f6de492b17
pedrojovanuzzi <138884556+pedrojovanuzzi@users.noreply.github.com>
2026-04-08 11:53:58 -0300

    Update SisPlano.ts

--- backend/src/entities/SisPlano.ts
+++ backend/src/entities/SisPlano.ts
@@ -10,4 +10,10 @@ export class SisPlano {
 
   @Column({ type: "varchar", length: 3, nullable: true })
   oculto!: "sim" | "nao";
+
+  @Column({ name: "AZ velup", type: "int", nullable: true })
+  velup!: number;
+
+  @Column({ name: "AZ veldown", type: "int", nullable: true })
+  veldown!: number;
 }
